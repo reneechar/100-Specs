a587648c3ec7f2a42dd511aa2446e0959a056bb8
Renee <rchar20@gmail.com>
2016-09-21 19:18:01 -1000

    canTalkAbout function passing tests

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -492,7 +492,13 @@ function purchaseLaptop(laptop) {
  * @return {Bool}
  *
  */
-
+function canTalkAbout(club) {
+	if(club === club_name) {
+		return false;
+	} else {
+		return true;
+	}
+}
 
 /* Step 30
  *
