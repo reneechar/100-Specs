8431f1f723d1393a6ad01ed9ab2f5467b5620187
Renee <rchar20@gmail.com>
2016-09-21 16:34:11 -1000

    DNA passes tests

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -195,7 +195,7 @@ var linuxFlavors = ["Gentoo", "Fedora", "Debian", "Slackware", "Red Hat", "Biebe
  * "cytosine"
  *
  */
-
+var DNA = ["nucleatides", "guanine", "adenine", "thymine", "cytosine"];
 
 /* Step 16
  *
