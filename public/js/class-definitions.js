da351ac3d041fd5f8bf59eaebebf2159a239d21f
Renee <rchar20@gmail.com>
2016-09-21 20:37:37 -1000

    nemo passes tests

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1110,7 +1110,7 @@ class Meal {
 
 // Create 2 different species of animals
 var george = new Animal('Monkey','male');
-var nemo;
+var nemo = new Animal('Fish','male');
 
 // Create 2 different vehicles
 var civic;
