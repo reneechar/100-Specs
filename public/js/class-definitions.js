8ab910211be5775f6afde71aeb543b4f0375d736
Renee <rchar20@gmail.com>
2016-09-21 16:38:26 -1000

    Beers passes tests

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -262,7 +262,14 @@ var scmList = ["git", "svn", "mercurial", "bazaar", "cvs"];
  *                  "Golden"
  *
  */
-
+var beers = {
+	IPA: 'Ale',
+	Lager: 'Strong',
+	Heffeweisen: 'German',
+	Stout: ['Thick','Dark'],
+	Porter: 'Bitter',
+	Ale: ['Light','Golden']
+};
 
 /* Step 20
  *
