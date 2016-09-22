d9a299f1c3b3d0351e0bc70fe53d4f82cfe12e39
Renee <rchar20@gmail.com>
2016-09-21 20:28:34 -1000

    House Class passes tests

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1061,7 +1061,11 @@ class Shoe {
  * @param {number} stories How many stories tall the house is
  */
 
-
+class House {
+	constructor(stories) {
+		this.stories = stories;
+	} 
+}
 /**
  * Step 57
  *
